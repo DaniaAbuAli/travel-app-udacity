cc6b30e88999c2c9895b2745950c200ad94cf9c3
DaniaAbuAli <daniaabuali12@gmail.com>
2025-02-05 12:37:55 +0200

    edit

--- _test_/server.test.js
+++ _test_/server.test.js
@@ -1,5 +1,8 @@
-import {app} from '../src/server/server'
+import {app,server} from '../src/server/server'
 import request from "supertest";
+afterAll(async () => {
+  server.close(); 
+});
 describe('GET /', () => {
     it('should return the index.html file', async () => {
       const response = await request(app).get('/');
